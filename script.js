53e746e27f66fb8bb38d43a4be3451f455d7e6f5
amir <shiodiyev.amir123@example.com>
2023-04-03 10:39:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// №1
+ 
+let arr = ['morgen','nikolay','alexander','yulduz','timati']
+
+let finded = arr.filter((name) => name.length >= 8) //тут или filter или find
+
+console.log(finded);
+
+
+
+
+
+
+
+
